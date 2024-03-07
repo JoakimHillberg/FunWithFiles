741dcd6f1954be9a56d80b76ef83a5def0ea564e
JoakimHillberg <joakim.hillberg@elev.ga.ntig.se>
2024-03-07 12:32:19 +0100

    Skapade klassen FindLongestWord där programmet läser igenom en fil med ett ord per rad och tittar vilket som är det längsta ordet i filen. Det ordet skrivs ut i slutet.

---
+++ src/FindLongestWord.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FindLongestWord {
+    // Attributes
+    String longestWord = "";
+    String fileName = "words.txt";
+    File myFile = new File(fileName);
+    Scanner fileScan;
+
+    public FindLongestWord() {
+        createFileObject();
+        findLongestWord();
+    }
+
+    // Methods
+    /*
+    * Creates a Scanner that reads from the words.txt file.
+    * If the words.txt file does not exist there is an error message.
+    */
+    public void createFileObject() {
+        try {
+            fileScan = new Scanner(myFile);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+        }
+    }
+
+    /*
+    * Reads one line at a time until there are no other lines.
+    * Checks if the length of the word on a line is longer than the previous longestWord.
+    * If it is the variable is changed to the newly found longestWord.
+    * At the end the program writes out what the longest word is.
+    */
+    public void findLongestWord() {
+        while (fileScan.hasNextLine()) {
+            String currentWord = fileScan.nextLine();
+            if (currentWord.length() > longestWord.length()) {
+                longestWord = currentWord;
+            }
+        }
+        fileScan.close();
+        System.out.println("The longest word is: " + longestWord);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        new WriteOverOrNot();
+        // new WriteOverOrNot();
+        new FindLongestWord();
     }
 }
